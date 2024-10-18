902632cb86b8b67cd0f62a8ef5d9e723195bb978
우경훈 <khwoo@sangsang.farm>
2024-10-18 16:49:36 +0900

    fix: fix tailwind font
    - fix line-height problem
      - text cut if line height is set.

--- marathoner/src/screens/Auth/LoginScreen.tsx
+++ marathoner/src/screens/Auth/LoginScreen.tsx
@@ -2,13 +2,6 @@ import React from 'react';
 import {FC} from 'react';
 import {useTranslation} from 'react-i18next';
 import {Text, View} from 'react-native';
-import Animated, {
-    useSharedValue,
-    useAnimatedStyle,
-    withTiming,
-    withRepeat,
-    withSequence,
-} from 'react-native-reanimated';
 import GreenFilledButton from '~/components/Button/Filled/GreenFilledButton';
 import RightArrowSVG from '~/assets/images/icons/goliath_btn_arrow.svg';
 import useLogin from '~/hooks/Login/useLogin';
@@ -22,20 +15,19 @@ const LoginScreen: FC = () => {
     return (
         <>
             <TopLineDecoration />
-            <View className="h-[100px] w-2/5 top-[100px] left-[40px] bg-diagonal-stripes bg-diagonal-stripes-size" />
             <View className="flex-1 items-center justify-center">
                 <View className="flex flex-col items-start border-t-4 pt-4 border-white">
                     <MarathonLogo height={60} width={60} />
-                    <Text className="text-white text-[12px] font-plex-light mt-4">
+                    <Text className="text-white text-sm font-plex-light mt-4">
                         {t('login.body')}
                     </Text>
-                    <Text className="text-white text-[30px] border-b border-white pb-1 font-plex-regular">
+                    <Text className="text-white text-3xl border-b border-white pb-1 font-plex-regular self-stretch">
                         {t('login.header')}
                     </Text>
                     <GreenFilledButton
                         className="mt-6 flex flex-row px-2 pr-8 items-center gap-6"
                         onPress={auth}>
-                        <Text className="font-plex-bold text-[50px]">
+                        <Text className="font-plex-bold text-5xl">
                             {t('login.button')}
                         </Text>
                         <View className="animate-to-right">
--- marathoner/tailwind.config.js
+++ marathoner/tailwind.config.js
@@ -17,14 +17,6 @@ module.exports = {
             colors: {
                 'vivid-green': '#B4FD2E',
             },
-            fontSize: {
-                tiny: '0.625rem',
-                small: '0.75rem',
-                base: '1rem',
-                large: '1.25rem',
-                extralarge: '1.5rem',
-                huge: '2rem',
-            },
             keyframes: {
                 'to-right': {
                     '0%': {transform: 'translateX(0)'},
@@ -34,6 +26,23 @@ module.exports = {
             animation: {
                 'to-right': 'to-right 1s ease-in-out infinite alternate',
             },
+        },
+        fontSize: {
+            // regenerate text sizes without line-height
+            xxs: '.625rem',
+            xs: '.75rem',
+            sm: '.875rem',
+            base: '1rem',
+            lg: '1.125rem',
+            xl: '1.25rem',
+            '2xl': '1.5rem',
+            '3xl': '1.875rem',
+            '4xl': '2.25rem',
+            '5xl': '3rem',
+            '6xl': '4rem',
+            '7xl': '5rem',
+            '8xl': '6rem',
+            '9xl': '8rem',
         },
     },
     plugins: [],
